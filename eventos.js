e1c4f30cf6dce6200f485918bacca15ca0c389ad
laura930 <174218186+laura930@users.noreply.github.com>
2024-08-16 00:01:12 -0300

    alerta div funciona

---
+++ eventos.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+let selectDiv= document.getElementById("div")
+
+selectDiv.addEventListener("click", function(){
+    alert('Hola, Soy el div');
+})
+});
+
+/* function saludoDiv(){
+alert('Hola, Soy el div')
+} */
+  
+
+
